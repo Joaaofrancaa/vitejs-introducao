5394e3c9766c40dd40fabf02e58087c763b832cd
Luis Fernando Rodrigues Bueno <77luisnando@gmail.com>
2026-02-27 22:46:10 -0300

    27/02

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import ReactDOM from 'react-dom/client'; // Por que tem ponto e vírgula no final desse import. 27/02/2026 ; 22:42
+import { BrowserRouter } from 'react-router-dom'; // Por que tem ponto e vírgula no final desse import. 27/02/2026 ; 22:42
+
+import './estilos/reset.css'
+import './estilos/globals.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
